1fa4918d87e7d2a71ed2d7b5b95ea72091d2b80a
우정 <woojeong1279@gmail.com>
2025-08-29 14:40:32 +0900

    보스 1

---
+++ Assets/Scripts/game_manager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static public GameManager Instance;
+
+    public GameObject boss;
+
+
+    [Header("# Player")]
+    public float playerHP;
+    public float playerMaxHP = 10;
+    public float playerATK;
+    public float PP;  //parrying point
+    public GameObject player;
+
+    public bool playerDie;
+
+    [Header("# Boss")]
+    public float bossHP;
+    public float bossMaxHP = 120;
+    public float bossATK;
+    public float bossHP2 = 60;  //phase2 hp
+
+    public float bossCoolTime = 3;
+    public bool bossDie;
+
+    [Header("# Others")]
+    public float GameTime;
+
+
+    void Awake(){
+        Instance = this;
+    }
+}
