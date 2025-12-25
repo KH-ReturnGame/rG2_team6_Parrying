afcb07d57b86f876981b8b8e4405cd6d5a56b2ae
topill992 <juhyeogi413@gmail.com>
2025-12-26 01:41:49 +0900

    일반 공격

--- Assets/Scripts/Player/parrying/attack.cs
+++ Assets/Scripts/Player/parrying/attack.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using System.Collections;
-
 
 public class PlayerAttack : MonoBehaviour
 {
@@ -12,7 +10,18 @@ public class PlayerAttack : MonoBehaviour
         public float active = 0.1f; // 공격 활성화 딜레이
         public float recovery = 1f; // 후딜레이
     }
+
     [Header("입력")]
     public KeyCode attackKey = KeyCode.E;
     public float comboBuffer = 0.2f; // 다음타 입력 버퍼
+
+    private AttackStep step = new AttackStep(); // 기본 damage=2 포함
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(attackKey))
+        {
+            Debug.Log($"Attack Damage = {step.damage}");
+        }
+    }
 }
