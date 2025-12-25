6e3f8ca746a102d31f424821a3e6e3cc343c8d8e
mugglewoojin <mugglewoojin@gmail.com>
2025-12-25 13:52:42 +0900

    새 씬 만듦

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -6,12 +6,16 @@ public class PlayerMove : MonoBehaviour
     public float HP;
     public Rigidbody2D rigid;
     public bool CanDash = true;
+
+    public Animator animator;
     
     void Start(){
         
         CanDash = true;
         rigid = GetComponent<Rigidbody2D>();
         HP = GameManager.Instance.playerHP;
+
+        animator = GetComponent<Animator>();
     }
 
     void Update(){
@@ -30,24 +34,32 @@ public class PlayerMove : MonoBehaviour
     }
 
     void move(){
-        transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal"), 0));
+        //transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal"), 0));
         //rigid.AddForce(Vector2.right * Input.GetAxis("Horizontal") * GameManager.Instance.PlayerSpeed, ForceMode2D.Impulse);
+
+        rigid.linearVelocityX = Input.GetAxisRaw("Horizontal") * 3f;
+        
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            animator.SetBool("IsWalking", true);
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            animator.SetBool("IsWalking", true);
+        }
+        else{
+            animator.SetBool("IsWalking", false);
+        }
     }
 
     IEnumerator Dash(){
         CanDash = false;
         Vector2 dir = Vector2.zero;
         if(Input.GetKey(KeyCode.RightArrow)){
-            rigid.AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+            rigid.AddForce(Vector2.right * 30, ForceMode2D.Impulse);
         }
         else if(Input.GetKey(KeyCode.LeftArrow)){
-            rigid.AddForce(Vector2.left * 10, ForceMode2D.Impulse);
+            rigid.AddForce(Vector2.left * 30, ForceMode2D.Impulse);
         }
-        else{
-            rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
-
-        }
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.2f);
         CanDash = true;
     }
 }
