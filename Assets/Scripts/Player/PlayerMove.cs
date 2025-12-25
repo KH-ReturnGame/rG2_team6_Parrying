09efd4806477c158c17e219e99033028970519dc
mugglewoojin <mugglewoojin@gmail.com>
2025-12-25 22:44:08 +0900

    플레이어 이동 애니메이션

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -7,12 +7,15 @@ public class PlayerMove : MonoBehaviour
     public Rigidbody2D rigid;
     public bool CanDash = true;
     public bool cannotmove = false;
+    public SpriteRenderer sprite;
+    public Animator animator;
     
     void Start(){
         
         CanDash = true;
         rigid = GetComponent<Rigidbody2D>();
         HP = GameManager.Instance.playerHP = 100;
+        sprite.GetComponent<SpriteRenderer>();
     }
 
     void Update(){
@@ -29,6 +32,18 @@ public class PlayerMove : MonoBehaviour
     }
 
     void move(){
-        rigid.linearVelocityX = Input.GetAxis("Horizontal") * 10;
+        rigid.linearVelocityX = Input.GetAxisRaw("Horizontal") * 10;
+
+        if(Input.GetKey(KeyCode.RightArrow)){
+            sprite.flipX = false;
+            animator.SetBool("isWalking", true);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow)){
+            sprite.flipX = true;
+            animator.SetBool("isWalking", true);
+        }
+        else{
+            animator.SetBool("isWalking", false);
+        }
     }
 }
