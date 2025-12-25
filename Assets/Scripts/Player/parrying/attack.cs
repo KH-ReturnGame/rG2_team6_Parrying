d981ddd5eb98ef6bca1b63d7142aa006ed0f1ec4
mugglewoojin <mugglewoojin@gmail.com>
2025-12-26 02:11:43 +0900

    '시연용' 완성품

--- Assets/Scripts/Player/parrying/attack.cs
+++ Assets/Scripts/Player/parrying/attack.cs
@@ -6,34 +6,23 @@ public class PlayerAttack : MonoBehaviour
     [System.Serializable]
     public class AttackStep
     {
-<<<<<<< HEAD
-        public int damage = 2; // ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+        public int damage = 2;          // ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
         public float activeTime = 0.3f; // ï¿½ï¿½ï¿½ï¿½ È°ï¿½ï¿½ ï¿½Ã°ï¿½
-        public float active = 0.1f; // ï¿½ï¿½ï¿½ï¿½ È°ï¿½ï¿½È­ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
-        public float recovery = 1f; // ï¿½Äµï¿½ï¿½ï¿½ï¿½ï¿½
-    }
-    [Header("ï¿½Ô·ï¿½")]
-    public KeyCode attackKey = KeyCode.E;
-    public float comboBuffer = 0.2f; // ï¿½ï¿½ï¿½ï¿½Å¸ ï¿½Ô·ï¿½ ï¿½ï¿½ï¿½ï¿½
-}
-=======
-        public int damage = 2;          // °ø°Ý µ¥¹ÌÁö
-        public float activeTime = 0.3f; // °ø°Ý È°¼º ½Ã°£
-        public float active = 0.1f;     // °ø°Ý È°¼ºÈ­ µô·¹ÀÌ
-        public float recovery = 1f;     // ÈÄµô·¹ÀÌ
+        public float active = 0.1f;     // ï¿½ï¿½ï¿½ï¿½ È°ï¿½ï¿½È­ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+        public float recovery = 1f;     // ï¿½Äµï¿½ï¿½ï¿½ï¿½ï¿½
     }
 
-    [Header("ÀÔ·Â")]
+    [Header("ï¿½Ô·ï¿½")]
     public KeyCode attackKey = KeyCode.E;
     public float comboBuffer = 0.2f;
 
-    [Header("ÄðÅ¸ÀÓ")]
+    [Header("ï¿½ï¿½Å¸ï¿½ï¿½")]
     [SerializeField] private float attackCooldown = 4f;
 
     private bool canAttack = true;
     private Coroutine cooldownCo;
 
-    private AttackStep step = new AttackStep(); // ±âº» damage=2 
+    private AttackStep step = new AttackStep(); // ï¿½âº» damage=2 
     private void Update()
     {
         if (Input.GetKeyDown(attackKey) && canAttack)
@@ -44,6 +33,7 @@ public class PlayerAttack : MonoBehaviour
 
             if (cooldownCo != null) StopCoroutine(cooldownCo);
             cooldownCo = StartCoroutine(AttackCooldownRoutine());
+            GameManager.Instance.bossHP -= 2;
         }
     }
 
@@ -55,4 +45,3 @@ public class PlayerAttack : MonoBehaviour
         cooldownCo = null;
     }
 }
->>>>>>> parrying
